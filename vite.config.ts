a1a07213a6e8d4569edd4d60abead5976affc8a0
robannable <rob.annable@gmail.com>
2025-01-21 22:26:14 +0000

    live demo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/turbines/',  // Match your repository name
+  base: '/turbines/',  // Should match your repository name
 })
